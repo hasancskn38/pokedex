ee7cc7f7f5ca84edab655419d2109e9f05d8f20c
hasancskn38 <95541333+hasancskn38@users.noreply.github.com>
2023-01-17 18:21:32 +0100

    First Change

---
+++ script.js
@@ -0,0 +1,99 @@
+// Global Variables, that are declared so that they can be used everywhere
+let currentPokemon;
+let allPokemons = [];
+
+// Get the Data from the API with an async function what means that the function does not run synchronous like normal functions
+async function loadPokedex() {
+    // for loop to be able to go through every pokemon
+    for (let i = 1; i < 152; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
+        // fetch the data with await fetch(url), await is used to wait for a promise
+        let response = await fetch(url);
+        // declare a variable that stands for every pokemon
+        currentPokemon = await response.json();
+        // Push the Pokemons in a global Array to have Access to them all the time
+        allPokemons.push(currentPokemon);
+    }
+    // call the renderPokemons() function to display all the pokemons
+    renderPokemons();
+}
+
+
+// Render Pokemon Function
+function renderPokemons() {
+    // for loop to go through the allPokemons Array where we pushed all the pokemons that we got from the async function
+    for (let i = 0; i < allPokemons.length; i++) {
+        let onePokemon = allPokemons[i];
+        let pokemonCards = document.getElementById('pokemon-cards');
+        pokemonCards.innerHTML  += generatePokemonCards(i, onePokemon);
+        changeColor(i);
+    }
+}
+
+
+// Search Pokemon Function
+function searchPokemon() {
+    // get the value of the input field
+    let input = document.getElementById('input').value;
+    let cards = document.getElementById('pokemon-cards');
+    // convert input into lowercase and the trim method removes whitespace from both sides of a string
+    input = input.toLowerCase().trim();
+    cards.innerHTML = '';
+    for (let i = 0; i < 151; i++) {
+        // declaring a variable for the pokemon names
+        let pokemonName = allPokemons[i]['name'];
+        /* if condition to check if the input field contains a letter that a pokemon has and if so it should run
+         the generatePokemonCards Function to display the pokemon which has the letters from the input field */
+        if(pokemonName.toLowerCase().includes(input)) {
+            cards.innerHTML += generatePokemonCards(i, allPokemons[i]);
+            // Solved the changeColor Problem by adding the changeColor Function inside the for loop
+            changeColor(i);
+        } 
+    }
+}
+
+
+// Open Pop Up Function
+function openPopUp(i) {
+    const onePokemon = allPokemons[i];
+    let popUp = document.getElementById('pokemon-cards-popup');
+    popUp.classList.remove('d-none');
+    popUp.innerHTML = generatePopUpHTML(i, onePokemon);
+    document.getElementById('overlay').classList.remove('d-none');
+    document.getElementById('header').classList.add('d-none');
+    /*Add the overflow hidden property to the body 
+    so that you cant scroll if you opened a popup*/
+    document.querySelector('body').classList.add('hidden');
+}
+    
+
+// Close Pop Up Function
+function closePopUp() {
+    let popUp = document.getElementById('pokemon-cards-popup');
+    popUp.classList.add('d-none');
+    document.getElementById('overlay').classList.add('d-none');
+    document.getElementById('header').classList.remove('d-none');
+    /*change the overflow hidden property to auto again so that
+    you can scroll again after closing the popup*/
+    document.querySelector('body').classList.remove('hidden');
+}
+
+
+// Previous Pokemon Function 
+function prePokemon(i) {
+    if(i == 0) {
+        openPopUp(150)
+    } else {
+    openPopUp(i-1);
+    }
+}
+
+// Next Pokemon Function
+function nextPokemon(i) {
+    if(i == 150) {
+        openPopUp(0)
+    } else {
+    openPopUp(i+1);
+    }
+}
+
---
+++ template.js
@@ -0,0 +1,163 @@
+function generatePokemonCards(i, onePokemon) {
+    return /*html*/`
+       <div id="pokemon-card-${i}" onclick="openPopUp(${i})" class="pokemon-card cursor">
+       <div class="pokemon-card-top">
+       <h2 class="pokemon-name" id="pokemon-name-${i}">${onePokemon['name']}</h2>
+       <h2 id="pokemon-id">#${i+1}</h2>
+       </div>
+       <div id="pokemon-card-bottom" class="pokemon-card-bottom">
+       <span class="pokemon-type" id="pokemon-type-${i}">${onePokemon['types']['0']['type']['name']}</span> <br>    
+       <img src="${onePokemon['sprites']['other']['official-artwork']['front_default']}" id="pokemon-image" alt="">
+       </div>
+       </div>
+       </div>
+       `;
+}
+
+
+function generatePopUpHTML(i, onePokemon) {
+    return /*html*/`
+        <div class="popup-wrap">
+        <!-- To solve the Problem with the background-color of the pop up version, the solution was to give the pop up container 
+        a class name with bg and afterwards the variable of what is displayed in the innerHTML of type, so every pop up gets his exact background-color -->
+        <div id="pokemon-card-${i}" class="popup-container bg-${onePokemon['types']['0']['type']['name']}">
+        <button class="cursor" onclick="closePopUp()">X</button>
+        <!-- PopUp Top Start -->
+        <div id="popup-top" class="popup-top">
+        <div class="top-info">
+        <p class="pokemon-id" id="pokemon-id-${i}">#${i+1}</p>
+        <h1 class="pokemon-name" id="pokemon-name-${i}">${onePokemon['name']}</h1>
+        <p id="popup-type" class="pokemon-type">${onePokemon['types']['0']['type']['name']}</p> <br>
+
+        <br>
+        </div>
+
+        <div class="image-container">
+        <img src="${onePokemon['sprites']['other']['official-artwork']['front_default']}" id="pokemon-image" alt="">
+        </div>
+
+        <div class="next-image-container">
+        <h3><button onclick="prePokemon(${i});"><img class="left-arrow" src="img/left-arrow.png" alt=""></button></h3>
+        <h3><button onclick="nextPokemon(${i});"><img class="right-arrow" src="img/left-arrow.png" alt=""></button></h3>
+        </div>
+
+        <div class="height-weight-container">
+        <h3>Weight:${onePokemon['weight']}</h3>
+        <h3>Height:${onePokemon['height']}</h3>
+        </div>
+
+        </div>
+        <!-- PopUp Top End -->
+
+        <!-- PopUp Bottom Start -->
+        <div class="popup-bottom">
+
+        <div class="base-stats-heading">
+        <h3>Base Stats</h3>
+        </div>
+
+        <hr>
+        <!-- PopUp Bottom End -->
+        <div class="stats-container">
+            <div class="stats-left">
+                <p class="stats-name">HP</p>
+                <p class="stats-name">Attack</p>
+                <p class="stats-name">Defense</p>
+                <p class="stats-name">Special-Attack</p>
+                <p class="stats-name">Special-Defense</p>
+                <p class="stats-name">Speed</p>
+            </div>
+
+            <div class="stats-center">
+                <p>${onePokemon['stats'][0]['base_stat']}</p>
+                <p>${onePokemon['stats'][1]['base_stat']}</p>
+                <p>${onePokemon['stats'][2]['base_stat']}</p>
+                <p>${onePokemon['stats'][3]['base_stat']}</p>
+                <p>${onePokemon['stats'][4]['base_stat']}</p>
+                <p>${onePokemon['stats'][5]['base_stat']}</p>
+            </div>
+
+            <div class="stats-right">
+        
+                <div class="progress-parent">
+                <progress value="${onePokemon['stats'][0]['base_stat']}" max="200"></progress>
+                </div>
+                <div class="progress-parent">
+                <progress value="${onePokemon['stats'][1]['base_stat']}" max="200"></progress>
+                </div>
+                <div class="progress-parent">
+                <progress value="${onePokemon['stats'][2]['base_stat']}" max="200"></progress>
+                </div>
+                <div class="progress-parent">
+                <progress value="${onePokemon['stats'][3]['base_stat']}" max="200"></progress>
+                </div>
+                <div class="progress-parent">
+                <progress value="${onePokemon['stats'][4]['base_stat']}" max="200"></progress>
+                </div>
+                <div class="progress-parent">
+                <progress value="${onePokemon['stats'][5]['base_stat']}" max="200"></progress>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    </div>`
+}
+
+
+// Change the Background-Color based on Pokemon Type
+function changeColor(i) {
+    let type = document.getElementById(`pokemon-type-${i}`);
+    let card = document.getElementById(`pokemon-card-${i}`);
+    if(type.innerHTML == 'grass') {
+        card.classList.add('bg-grass');
+    }
+    if(type.innerHTML == 'water') {
+        card.classList.add('bg-water');
+    }
+    if(type.innerHTML == 'bug') {
+        card.classList.add('bg-bug');
+    }
+    if(type.innerHTML == 'normal') {
+        card.classList.add('bg-normal');
+    }
+    if(type.innerHTML == 'grass') {
+        card.classList.add('bg-grass');
+    }
+    if(type.innerHTML == 'fire') {
+        card.classList.add('bg-fire');
+    }
+    if(type.innerHTML == 'electric') {
+        card.classList.add('bg-electric');
+    }
+    if(type.innerHTML == 'poison') {
+        card.classList.add('bg-poison');
+    }
+    if(type.innerHTML == 'fairy') {
+        card.classList.add('bg-fairy');
+    }
+    if(type.innerHTML == 'ground') {
+        card.classList.add('bg-ground');
+    }
+    if(type.innerHTML == 'fighting') {
+        card.classList.add('bg-fighting');
+    }
+    if(type.innerHTML == 'psychic') {
+        card.classList.add('bg-psychic');
+    }
+    if(type.innerHTML == 'rock') {
+        card.classList.add('bg-rock');
+    }
+    if(type.innerHTML == 'ghost') {
+        card.classList.add('bg-ghost');
+    }
+    if(type.innerHTML == 'ice') {
+        card.classList.add('bg-ice');
+    }
+    if(type.innerHTML == 'rock') {
+        card.classList.add('bg-rock');
+    }
+    if(type.innerHTML == 'dragon') {
+        card.classList.add('bg-dragon');
+    }
+}
